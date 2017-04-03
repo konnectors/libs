f13eba288c081fd686c3f74051f8efcdd1b4d3ca
doubleface <christophe@thiriot.xyz>
2017-04-03 21:22:42 +0200

    Init

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'base_konnector': require('./base_konnector'),
+    'cozy-client-js-stub': require('./cozy-client-js-stub'),
+    'cozyclient': require('./cozyclient'),
+    'fetcher': require('./fetcher'),
+    'filter_existing': require('./filter_existing'),
+    'gettoken': require('./gettoken'),
+    'manifest': require('./manifest'),
+    'naming': require('./naming'),
+    'save_data_and_file': require('./save_data_and_file')
+}
