357db95f47c6b356331c4cfc33635aa75818688c
Yannick Lohse <contact@yannick-lohse.fr>
2017-04-21 14:55:31 +0200

    feat: link bank operations

---
+++ models/bankoperation.js
@@ -0,0 +1,52 @@
+const cozy = require('../cozyclient')
+const moment = require('moment')
+const DOCTYPE = 'io.cozy.bankoperations'
+
+module.exports = {
+  displayName: 'Bank Operation',
+  all (selector, callback) {
+    if (!selector) selector = {date: {'$gt': 0}}
+
+    cozy.data.defineIndex(DOCTYPE, ['date'])
+    .then((index) => {
+      return cozy.data.query(index, {'selector': selector})
+    })
+    .then(operations => {
+      operations = operations.map(operation => {
+        operation.date = moment(operation.date)
+        return operation
+      })
+      callback(null, operations)
+    })
+    .catch(err => {
+      callback(err)
+    })
+  },
+  // Set binary of given file (represented by its id) to the current operation
+  setBinaryFromFile (operationId, fileId, callback) {
+    File.find(fileId, (err, file) => {
+      if (err) return callback(err)
+
+      if (file && file.binary && file.binary.file) {
+          let attributes = {
+            binary: {
+              file: file.binary.file,
+              fileName: file.name,
+              fileMime: file.mime
+            }
+          }
+
+          cozy.data.updateAttributes(DOCTYPE, operationId, attributes)
+          .then(updated => {
+            callback(updated)
+          })
+          .catch(err => {
+            callback(err)
+          })
+      }
+      else {
+        callback(new Error(`No binary for this file ${fileId}`))
+      }
+    })
+  }
+}
