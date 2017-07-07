370b2bb2153f5a532d5b0ff38ed0fe3272e70b21
doubleface <christophe@thiriot.xyz>
2017-07-07 10:48:17 +0200

    Display the types in an associated color

--- libs/logger.js
+++ libs/logger.js
@@ -3,13 +3,13 @@ util.inspect.defaultOptions.maxArrayLength = null
 util.inspect.defaultOptions.colors = true
 
 const env = process.env.NODE_ENV
-const formats = {
+const env2formats = {
   '': prodFormat,
   production: prodFormat,
   development: devFormat,
   standalone: devFormat
 }
-const format = formats[env]
+const format = env2formats[env]
 
 if (!format) console.error('Error while loading the logger')
 
@@ -29,5 +29,17 @@ function devFormat (type, message, label) {
 
   let formatlabel = ` : "${label}" `
   if (label === undefined) formatlabel = ``
-  return `${type}${formatlabel} : ${formatmessage}`
+
+  const type2color = {
+    debug: 3, // yellow
+    info: 4,  // blue
+    error: 1, // red
+    ok: 2     // green
+  }
+
+  let formattype = type
+  if (type2color[type]) {
+    formattype = `\u001b[3${type2color[type]}m${type}\u001b[0m`
+  }
+  return `${formattype}${formatlabel} : ${formatmessage}`
 }
