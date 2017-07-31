98ccab3efd3b18b4eab2b2535689599a89044855
m4dz <code@m4dz.net>
2017-07-31 17:29:28 +0200

    [fix] return encoded 2FA message (useful for tests)

--- errors.js
+++ errors.js
@@ -9,6 +9,7 @@ const MSG = {
 function requireTwoFactor(extras = {}) {
   const extrasStr = Buffer(JSON.stringify(extras), 'binary').toString('base64')
   log('error', `${MSG.TWOFACTOR}||${extrasStr}`)
+  return extrasStr
 }
 
 module.exports = {
