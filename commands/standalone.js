557850db8f5f6231cb3605fe8d5cd95b8d04d00f
doubleface <christophe@thiriot.xyz>
2017-08-06 09:31:14 +0200

    feat: added node-replay

--- commands/standalone.js
+++ commands/standalone.js
@@ -6,4 +6,9 @@ if (!process.env.DEBUG) process.env.DEBUG = '*'
 process.env.COZY_FIELDS = '{}'
 
 const filename = process.argv[2] || 'index.js'
+
+// assign default value of replay to bloody which cancels it
+process.env.REPLAY = process.env.REPLAY ? process.env.REPLAY : 'bloody'
+require('replay')
+
 require(require('path').resolve(filename))
