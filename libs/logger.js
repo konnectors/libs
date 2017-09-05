39999030a5c798ceac90a1da72399cf010b3a4de
doubleface <christophe@thiriot.xyz>
2017-09-05 14:11:38 +0200

    fix: try to display better error message in logs

--- libs/logger.js
+++ libs/logger.js
@@ -29,6 +29,8 @@ log.namespace = function (namespace) {
 }
 
 function prodFormat (type, message, label, namespace) {
+  // properly display error messages
+  if (message.stack) message = message.stack
   return JSON.stringify({ time: new Date(), type, message, label, namespace })
 }
 
