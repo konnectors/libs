a279eba52f6b9bc762f1c82faaa43b59029b2955
Patrick Browne <pt.browne@gmail.com>
2017-10-23 11:27:46 +0200

    ✨ feat: add error codes

--- helpers/errors.js
+++ helpers/errors.js
@@ -12,7 +12,35 @@ function requireTwoFactor (extras = {}) {
   return extrasStr
 }
 
+/**
+ * The konnector could not login
+ * @type {String}
+ */
+const LOGIN_FAILED = 'LOGIN_FAILED'
+
+/**
+ * The folder specified as folder_to_save does not exist (checked by BaseKonnector)
+ * @type {String}
+ */
+const NOT_EXISTING_DIRECTORY = 'NOT_EXISTING_DIRECTORY'
+
+/**
+ * The vendor's website is down
+ * @type {String}
+ */
+const VENDOR_DOWN = 'VENDOR_DOWN'
+
+/**
+ * There was an unexpected error, please take a look at the logs to know what happened
+ * @type {String}
+ */
+const USER_ACTION_NEEDED = 'USER_ACTION_NEEDED'
+
 module.exports = {
   MSG: MSG,
+  LOGIN_FAILED,
+  NOT_EXISTING_DIRECTORY,
+  VENDOR_DOWN,
+  USER_ACTION_NEEDED,
   requireTwoFactor: requireTwoFactor
 }
