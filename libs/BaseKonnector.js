5f9f64bfa2061979ab0fa827c1fb4012800ece2b
Grégory Le Garec <gregory.legarec@gmail.com>
2017-10-30 17:31:16 +0100

    feat: process.exit on terminate() 📝
    To ensure that our log messages are actually displayed, we use a setImmediate() call.
    To prevent future promise chaining, we encapsulate the whole process into a Promise which will never be fulfilled.

--- libs/BaseKonnector.js
+++ libs/BaseKonnector.js
@@ -154,10 +154,18 @@ class baseKonnector {
    * @param  {string} message - The error code to be saved as connector result see [docs/ERROR_CODES.md]
    */
   terminate (message) {
-    // The error log is also sent to be compatible with older versions of the cozy stack
-    // For version of the stack older than 18bcbd5865a46026de6f794f661d83d5d87a3dbf
-    log('error', message)
-    log('critical', message)
+    // Encapsulating in a Promise allows to prevent then() calls before
+    // process.exit is actually called.
+    return new Promise(() => {
+      // The error log is also sent to be compatible with older versions of the cozy stack
+      // For version of the stack older than 18bcbd5865a46026de6f794f661d83d5d87a3dbf
+      log('error', message)
+      log('critical', message)
+      // Allow asynchronous calls to end, for example, previous log calls.
+      // To breack promise chaining and avoid then() calls to be made,
+      // The call is encapsulated in a promise, see above.
+      setImmediate(() => process.exit(1))
+    })
   }
 }
 
