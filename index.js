dacd73c4cf03fbbf1936f049003dacc09d0ecff6
doubleface <christophe@cozycloud.cc>
2018-01-03 15:01:36 +0100

    fix: add a deprecation warning for request alias

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 require('./libs/error')
 
 const requestFactory = require('./libs/request')
+const log = require('./libs/logger').namespace('cozy-konnector-libs')
 
 module.exports = {
   BaseKonnector: require('./libs/BaseKonnector'),
   cozyClient: require('./libs/cozyclient'),
   errors: require('./helpers/errors'),
-  log: require('./libs/logger'),
+  log,
   saveFiles: require('./libs/saveFiles'),
   saveBills: require('./libs/saveBills'),
   linkBankOperations: require('./libs/linkBankOperations'),
   addData: require('./libs/addData'),
   filterData: require('./libs/filterData'),
   updateOrCreate: require('./libs/updateOrCreate'),
-  request: requestFactory, // keep the old alias
+  request: deprecate(requestFactory, 'Use requestFactory instead of request. It will be removed in cozy-konnector-libs@4'),
   requestFactory,
   retry: require('bluebird-retry')
+}
+
+function deprecate (wrapped, message) {
+  return function () {
+    log('warn', message)
+    return wrapped.apply(this, arguments)
+  }
 }
