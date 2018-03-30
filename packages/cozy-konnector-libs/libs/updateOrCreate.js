e614c971f9bdd187ee5ccc90c5fb03d84dd4ec3a
Patrick Browne <pt.browne@gmail.com>
2018-03-30 17:55:46 +0200

    🐝 fix: remove deprecation warning for updateOrCreate as there are no clear doc on how to use shouldSave/shouldUpdate

--- packages/cozy-konnector-libs/libs/updateOrCreate.js
+++ packages/cozy-konnector-libs/libs/updateOrCreate.js
@@ -18,7 +18,6 @@ const log = require('cozy-logger').namespace('updateOrCreate')
 const cozy = require('./cozyclient')
 
 module.exports = (entries = [], doctype, matchingAttributes = []) => {
-  log('warn', 'updateOrCreate is deprecated, define shouldSave/shouldUpdate')
   return cozy.data.findAll(doctype)
     .then(existings => bluebird.mapSeries(entries, entry => {
       log('debug', entry)
