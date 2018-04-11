816b6113488714bba59e4d9dd02f68a761889df8
doubleface <christophe@cozycloud.cc>
2018-04-11 21:13:16 +0200

    feat: replace saveBills by saveDocuments

--- packages/cozy-konnector-libs/src/libs/saveDocuments.js
+++ packages/cozy-konnector-libs/src/libs/saveDocuments.js
@@ -1,36 +1,39 @@
 /**
- * Combines the features of `saveFiles`, `filterData`, `addData` and `linkBankOperations` for a
- * common case: bills.
- * Will create `io.cozy.bills` objects. The default deduplication keys are `['date', 'amount', 'vendor']`.
- * You need the full permission on `io.cozy.bills`, full permission on `io.cozy.files` and also
- * full permission on `io.cozy.bank.operations` in your manifest, to be able to * use this function.
+ * Combines the features of `saveFiles`, `filterData`, `addData` and `linkBankOperations` for any
+ * doctype.
+ * Will create `io.cozy.bills` documents by default or any specified doctype.
+ * The default deduplication keys are `['date', 'amount', 'vendor']`.
+ * You need the full permission on the doctype, full permission on `io.cozy.files` and also
+ * full permission on `io.cozy.bank.operations` in your manifest, to be able to use this function.
  *
  * Parameters:
  *
  * - `documents` is an array of objects with any attributes :
  * - `fields` (object) this is the first parameter given to BaseKonnector's constructor
  * - `options` is passed directly to `saveFiles`, `hydrateAndFilter`, `addData` and `linkBankOperations`.
+ *      - `doctype` option has `io.cozy.bills value by default`
  *
  * ```javascript
- * const { BaseKonnector, saveBills } = require('cozy-konnector-libs')
+ * const { BaseKonnector, saveDocuments } = require('cozy-konnector-libs')
  *
  * module.exports = new BaseKonnector(function fetch (fields) {
  *   const documents = []
  *   // some code which fills documents
- *   return saveBills(documents, fields, {
- *     identifiers: ['vendorj']
+ *   return saveDocuments(documents, fields, {
+ *     identifiers: ['vendorj'],
+ *     doctype: 'io.cozy.bills'
  *   })
  * })
  * ```
  *
- * @module  saveBills
+ * @module  saveDocuments
  */
 
 const saveFiles = require('./saveFiles')
 const hydrateAndFilter = require('./hydrateAndFilter')
 const addData = require('./addData')
 const linkBankOperations = require('./linkBankOperations')
-const DOCTYPE = 'io.cozy.bills'
+const DEFAULT_DOCTYPE = 'io.cozy.bills'
 
 // Encapsulate the saving of Bills : saves the files, saves the new data, and associate the files
 // to an existing bank operation
@@ -41,12 +44,20 @@ module.exports = (entries, fields, options = {}) => {
     fields = { folderPath: fields }
   }
 
+  options.doctype = options.doctype || DEFAULT_DOCTYPE
+
   // Deduplicate on this keys
   options.keys = options.keys || ['date', 'amount', 'vendor']
 
   options.postProcess = function (entry) {
     if (entry.fileDocument) {
-      entry.invoice = `io.cozy.files:${entry.fileDocument._id}`
+      entry.$relationships = entry.$relationships || {}
+      entry.$relationships.document = {
+        data: {
+          _id: entry.fileDocument._id,
+          _type: 'io.cozy.files'
+        }
+      }
     }
     delete entry.fileDocument
     return entry
@@ -54,7 +65,7 @@ module.exports = (entries, fields, options = {}) => {
 
   const originalEntries = entries
   return saveFiles(entries, fields, options)
-    .then(entries => hydrateAndFilter(entries, DOCTYPE, options))
-    .then(entries => addData(entries, DOCTYPE, options))
-    .then(entries => linkBankOperations(originalEntries, DOCTYPE, fields, options))
+    .then(entries => hydrateAndFilter(entries, options.doctype, options))
+    .then(entries => addData(entries, options.doctype, options))
+    .then(entries => linkBankOperations(originalEntries, options.doctype, fields, options))
 }
