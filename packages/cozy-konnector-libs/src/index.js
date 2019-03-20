3787ee3d65243e71a3b5ba1e29a60272031b553c
doubleface <christophe@cozycloud.cc>
2019-03-20 15:18:12 +0100

    fix: add isomorphic-fetch sooner

--- packages/cozy-konnector-libs/src/index.js
+++ packages/cozy-konnector-libs/src/index.js
@@ -1,7 +1,7 @@
+require('isomorphic-fetch')
 const log = require('cozy-logger').namespace('cozy-konnector-libs')
 const requestFactory = require('./libs/request')
 const hydrateAndFilter = require('./libs/hydrateAndFilter')
-require('isomorphic-fetch')
 
 require('./libs/error')
 
