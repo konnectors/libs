bee4889a9c92cdaf2516cfb9826119ccc8ff5fbe
doubleface <christophe@cozycloud.cc>
2021-11-18 11:31:56 +0100

    fix: Regression in BaseKonnector unit test
    Now COZY_FIELDS needs an "account" attributes to have proper
    fields in the BaseKonnector, which is normal after all

--- packages/cozy-konnector-libs/src/libs/BaseKonnector.spec.js
+++ packages/cozy-konnector-libs/src/libs/BaseKonnector.spec.js
@@ -47,6 +47,7 @@ describe('finding folder path', () => {
 describe('run', () => {
   const envFields = {
     COZY_FIELDS: JSON.stringify({
+      account: 'testaccount',
       COZY_URL: 'http://cozy.tools:8080',
       fields: {
         login: 'mylogin',
