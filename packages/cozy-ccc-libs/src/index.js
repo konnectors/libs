88c50abde3e6fa48d5d50a846a6e3dee59d67628
doubleface <christophe@cozycloud.cc>
2023-01-17 17:54:04 +0100

    feat: copy from flagship application + package init

---
+++ packages/cozy-ccc-libs/src/index.js
@@ -0,0 +1,5 @@
+import { Bridge, MessengerInterface } from './bridge/bridgeInterfaces'
+import ContentScript from './ContentScript'
+import LauncherLib from './launcher'
+
+export { Bridge, MessengerInterface, ContentScript, LauncherLib }
