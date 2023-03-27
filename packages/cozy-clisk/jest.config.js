b84278d9a3b15af73bb6870d855cc1fa68e41e41
doubleface <christophe@cozycloud.cc>
2023-03-27 16:49:03 +0200

    fix: ignore p-timeout and p-wait-for in jest transforms
    These modules are real esm modules and should not be transformed

--- packages/cozy-clisk/jest.config.js
+++ packages/cozy-clisk/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/dist/']
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  transformIgnorePatterns: ['/node_modules/(?!(p-timeout|p-wait-for))']
 }
