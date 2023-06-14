238b1f0f5cc72dc1c4df9e808b99ac4e321447c9
doubleface <christophe@cozycloud.cc>
2023-06-14 14:07:42 +0200

    feat: Download files progressively only when needed
    Now the launcher will only have to call saveFiles directly for all the
    given files,  which will avoid to download all the files before calling
    saveFiles. (PR in flagship app to follow)
    
    The downloadAndFormatFile option callback has been added to allow
    saveFiles to download the corresponding file in the worker when needed
    
    The fetch of the existing files index is now done by the Launcher and
    the same index is used for every call to saveFiles of saveBills in the
    same konnector run, for performance reason.

--- packages/cozy-clisk/src/launcher/saveFiles.spec.js
+++ packages/cozy-clisk/src/launcher/saveFiles.spec.js
@@ -1,18 +1,116 @@
 import saveFiles from './saveFiles'
 
 describe('saveFiles', function () {
-  it('should save a file without subPath', async () => {
+  it('should not download a file if the file is already present', async () => {
+    const fileDocument = {
+      _type: 'io.cozy.files',
+      type: 'file',
+      data: 'already present file content',
+      dirId: '/test/folder/path',
+      name: 'old file name.txt',
+      cozyMetadata: {
+        sourceAccount: 'testsourceaccount',
+        sourceAccountIdentifier: 'testsourceaccountidentifier'
+      },
+      metadata: {
+        fileIdAttributes: 'old file name.txt'
+      }
+    }
+    const existingFilesIndex = new Map([['old file name.txt', fileDocument]])
+    const client = {
+      save: jest.fn(),
+      collection: () => ({
+        statByPath: jest.fn().mockImplementation(path => {
+          return { data: { _id: path } }
+        })
+      })
+    }
+
+    const downloadAndFormatFile = jest.fn()
+
+    const document = {
+      fileurl: 'https://myfile.txt',
+      filename: 'old file name.txt'
+    }
+    const result = await saveFiles(client, [document], '/test/folder/path', {
+      manifest: {
+        slug: 'testslug'
+      },
+      sourceAccount: 'testsourceaccount',
+      sourceAccountIdentifier: 'testsourceaccountidentifier',
+      fileIdAttributes: ['filename'],
+      existingFilesIndex,
+      downloadAndFormatFile
+    })
+
+    expect(downloadAndFormatFile).not.toHaveBeenCalled()
+
+    expect(result).toStrictEqual([
+      {
+        ...document,
+        fileDocument
+      }
+    ])
+  })
+  it('should download a file with fileurl and without filestream', async () => {
     const client = {
       save: jest.fn().mockImplementation(doc => ({
         data: doc
       })),
       collection: () => ({
         statByPath: jest.fn().mockImplementation(path => {
-          if (path === '/test/folder/path/file name.txt') {
-            return { data: null }
-          } else {
-            return { data: { _id: path } }
-          }
+          return { data: { _id: path } }
+        })
+      })
+    }
+
+    const downloadAndFormatFile = jest.fn().mockResolvedValue({
+      filestream: 'downloaded file content'
+    })
+
+    const document = {
+      fileurl: 'https://myfile.txt',
+      filename: 'file name.txt'
+    }
+    const result = await saveFiles(client, [document], '/test/folder/path', {
+      manifest: {
+        slug: 'testslug'
+      },
+      sourceAccount: 'testsourceaccount',
+      sourceAccountIdentifier: 'testsourceaccountidentifier',
+      fileIdAttributes: ['filename'],
+      existingFilesIndex: new Map(),
+      downloadAndFormatFile
+    })
+
+    const fileDocument = {
+      _type: 'io.cozy.files',
+      type: 'file',
+      data: 'downloaded file content',
+      dirId: '/test/folder/path',
+      name: 'file name.txt',
+      sourceAccount: 'testsourceaccount',
+      sourceAccountIdentifier: 'testsourceaccountidentifier',
+      metadata: {
+        fileIdAttributes: 'file name.txt'
+      }
+    }
+    expect(client.save).toHaveBeenCalledWith(fileDocument)
+    expect(result).toStrictEqual([
+      {
+        ...document,
+        fileDocument
+      }
+    ])
+  })
+  it('should save a file with filestream without subPath', async () => {
+    const client = {
+      save: jest.fn().mockImplementation(doc => ({
+        data: doc
+      })),
+      collection: () => ({
+        statByPath: jest.fn().mockImplementation(path => {
+          return { data: { _id: path } }
         })
       })
     }
@@ -25,7 +123,9 @@ describe('saveFiles', function () {
         slug: 'testslug'
       },
       sourceAccount: 'testsourceaccount',
-      sourceAccountIdentifier: 'testsourceaccountidentifier'
+      sourceAccountIdentifier: 'testsourceaccountidentifier',
+      fileIdAttributes: ['filename'],
+      existingFilesIndex: new Map()
     })
     const fileDocument = {
       _type: 'io.cozy.files',
@@ -34,10 +134,63 @@ describe('saveFiles', function () {
       dirId: '/test/folder/path',
       name: 'file name.txt',
       sourceAccount: 'testsourceaccount',
-      sourceAccountIdentifier: 'testsourceaccountidentifier'
+      sourceAccountIdentifier: 'testsourceaccountidentifier',
+      metadata: {
+        fileIdAttributes: 'file name.txt'
+      }
     }
     expect(client.save).toHaveBeenCalledWith(fileDocument)
     expect(result).toStrictEqual([
+      {
+        ...document,
+        fileDocument
+      }
+    ])
+  })
+  it('should save a file with qualification label', async () => {
+    const client = {
+      save: jest.fn().mockImplementation(doc => ({
+        data: doc
+      })),
+      collection: () => ({
+        statByPath: jest.fn().mockImplementation(path => {
+          return { data: { _id: path } }
+        })
+      })
+    }
+    const document = {
+      filestream: 'filestream content',
+      filename: 'file name.txt'
+    }
+    const result = await saveFiles(client, [document], '/test/folder/path', {
+      manifest: {
+        slug: 'testslug'
+      },
+      sourceAccount: 'testsourceaccount',
+      sourceAccountIdentifier: 'testsourceaccountidentifier',
+      fileIdAttributes: ['filename'],
+      existingFilesIndex: new Map(),
+      qualificationLabel: 'energy_invoice'
+    })
+    const fileDocument = {
+      _type: 'io.cozy.files',
+      type: 'file',
+      data: 'filestream content',
+      dirId: '/test/folder/path',
+      name: 'file name.txt',
+      sourceAccount: 'testsourceaccount',
+      sourceAccountIdentifier: 'testsourceaccountidentifier',
+      metadata: {
+        fileIdAttributes: 'file name.txt',
+        qualification: {
+          label: 'energy_invoice',
+          purpose: 'invoice',
+          sourceCategory: 'energy'
+        }
+      }
+    }
+    expect(client.save).toHaveBeenCalledWith(fileDocument)
+    expect(result).toEqual([
       {
         ...document,
         fileDocument
@@ -52,11 +205,7 @@ describe('saveFiles', function () {
       collection: () => ({
         createDirectoryByPath: jest.fn(),
         statByPath: jest.fn().mockImplementation(path => {
-          if (path === '/test/folder/path/file name.txt') {
-            return { data: null }
-          } else {
-            return { data: { _id: path } }
-          }
+          return { data: { _id: path } }
         })
       })
     }
@@ -70,7 +219,9 @@ describe('saveFiles', function () {
       },
       subPath: 'subPath',
       sourceAccount: 'testsourceaccount',
-      sourceAccountIdentifier: 'testsourceaccountidentifier'
+      sourceAccountIdentifier: 'testsourceaccountidentifier',
+      fileIdAttributes: ['filename'],
+      existingFilesIndex: new Map()
     })
     expect(client.save).toHaveBeenCalledWith(
       expect.objectContaining({
