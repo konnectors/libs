123633438d565b036ac8647470126d62dc077ffc
Crash-- <quentin.valmori@gmail.com>
2024-01-02 08:35:39 +0100

    fix: saveIdentity tries to take care of malformed data
    If strings are passed when we want array, we try to fix them. We also
    add warns in order to fix the root cause.

--- packages/cozy-clisk/src/launcher/saveIdentity.spec.js
+++ packages/cozy-clisk/src/launcher/saveIdentity.spec.js
@@ -1,4 +1,4 @@
-import saveIdentity from './saveIdentity'
+import saveIdentity, { formatIdentityContact } from './saveIdentity'
 
 describe('saveIdentity', function () {
   const client = {
@@ -15,7 +15,11 @@ describe('saveIdentity', function () {
 
   it('should save identity when no identical identity is found', async () => {
     const identity = {
-      email: 'test@mail.com'
+      email: [
+        {
+          address: 'test@mail.com'
+        }
+      ]
     }
 
     client.query.mockResolvedValue({
@@ -33,7 +37,11 @@ describe('saveIdentity', function () {
 
   it('should update identity when identity createdWith the same app and with the same identifier is found', async () => {
     const identity = {
-      email: 'test2@mail.com'
+      email: [
+        {
+          address: 'test2@mail.com'
+        }
+      ]
     }
 
     client.query.mockResolvedValue({
@@ -43,7 +51,11 @@ describe('saveIdentity', function () {
           _rev: 'testrev',
           _type: 'io.cozy.identities',
           contact: {
-            email: 'testprevious@mail.com'
+            email: [
+              {
+                address: 'testprevious@mail.com'
+              }
+            ]
           },
           identifier: 'testSourceAccountIdentifier'
         }
@@ -63,7 +75,11 @@ describe('saveIdentity', function () {
 
   it('should update identity with previous identity metadata', async () => {
     const identity = {
-      email: 'test2@mail.com'
+      email: [
+        {
+          address: 'test2@mail.com'
+        }
+      ]
     }
 
     client.query.mockResolvedValue({
@@ -73,7 +89,11 @@ describe('saveIdentity', function () {
           _rev: 'testrev',
           _type: 'io.cozy.identities',
           contact: {
-            email: 'testprevious@mail.com'
+            email: [
+              {
+                address: 'testprevious@mail.com'
+              }
+            ]
           },
           cozyMetadata: {
             createdByApp: 'testslug'
@@ -95,5 +115,40 @@ describe('saveIdentity', function () {
       _id: 'testid',
       _rev: 'testrev'
     })
+  })
+})
+
+describe('formatIdentityContact', () => {
+  it('should format (create the array) phone, address & mail if only strings', () => {
+    const contact = {
+      phone: '0601020304',
+      address: '1 rue de la paix',
+      email: 'foo@cozycloud.cc'
+    }
+    const formattedContact = formatIdentityContact(contact)
+
+    const expectedContact = {
+      phone: [{ number: contact.phone }],
+      address: [{ formattedAddress: contact.address }],
+      email: [{ address: contact.email }]
+    }
+
+    expect(formattedContact).toEqual(expectedContact)
+  })
+  it('should format (create the array) phone, address & mail if string or array', () => {
+    const contact = {
+      phone: [{ number: '0601020304' }],
+      address: '1 rue de la paix',
+      email: 'foo@cozycloud.cc'
+    }
+    const formattedContact = formatIdentityContact(contact)
+
+    const expectedContact = {
+      phone: contact.phone,
+      address: [{ formattedAddress: contact.address }],
+      email: [{ address: contact.email }]
+    }
+
+    expect(formattedContact).toEqual(expectedContact)
   })
 })
